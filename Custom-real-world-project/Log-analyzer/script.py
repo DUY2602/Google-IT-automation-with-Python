7825a898ea89bd0d32ba3c4d405042edfbcc38b9
daveduy2006@gmail.com <duydo7779@gmail.com>
2025-09-09 17:19:21 +0700

    Final update about Google IT automation with python

--- Custom-real-world-project/Log-analyzer/script.py
+++ Custom-real-world-project/Log-analyzer/script.py
@@ -1,11 +1,27 @@
 import re
 
-def count_log(log):
+def count_log():
+    log = input("What logging status do you want to check: (ERROR, INFO, WARNING) ")
     regex = rf"{log}"
+    with open ("sample.log", "r") as f:
+        count = 0
+        for line in f:
+            target = re.search(regex, line)
+            if target:
+                print(line)
+                count += 1
+    print(f"There are {count} ERROR occur")
+
+# count_log("ERROR")
+
+def get_date_status():
+    date = input("What date do you want to check:  (YYYY-MM-DD) ")
+    print(f"Listing the logging status for day: {date}")
+    regex = rf"{date}"
     with open ("sample.log", "r") as f:
         for line in f:
             target = re.search(regex, line)
             if target:
                 print(line)
 
-count_log("ERROR")
+get_date_status()
